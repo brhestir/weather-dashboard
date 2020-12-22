0fbc5bb0e1d5ef45e26d38b77d51eb2e9f3bfb89
Brian Hestir <brhestir@gmail.com>
2020-12-21 20:49:59 -0500

    Added api ajax call for test of basic functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,49 @@
 // script.js
+
+// When the document is ready, perform the following
+$(document).ready(function(){
+
+  // DOM VARIABLES
+
+  // JAVASCRIPT VARIABLES
+  var apiKey = "b4870574d339978807fdf94ae06ca36a"
+  var cityName = "Atlanta";
+  var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+  var cityDataResponse = [];
+  // FUNCTION DEFINITIONS
+  function init(){
+
+  }
+
+  function renderOneDayForecast(cityDataResponse){
+    $("#cityName").text(cityDataResponse.name);
+    $("#cityTemp").text("Temperature: " + cityDataResponse.main.temp)
+    $("#cityHumidity").text("Humidity: " + cityDataResponse.main.humidity)
+    $("#cityWindSpeed").text("Wind Speed: " + cityDataResponse.wind.speed)
+    // $("#cityUVIndex").text(cityDataResponse.)
+  }
+
+  function renderFiveDayForecast(){
+
+  }
+  // FUNCTION CALLS
+  // EVENT LISTENERS
+
+  $("#search-submit").on("click", getCityData());
+
+
+  function getCityData(){
+    console.log("getCityData Called");
+    $.ajax({
+      url: queryUrl,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+
+      renderOneDayForecast(response);
+
+    });
+
+  };
+
+});
